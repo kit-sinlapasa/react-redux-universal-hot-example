8b36983b157881d4b24c87275f9b71e93a1eac24
Krishna Sinlapasa <krishna.sinlapasa@gmail.com>
2025-06-20 22:33:24 +0700

    Fix error message for .babelrc parsing

--- server.babel.js
+++ server.babel.js
@@ -8,7 +8,7 @@ var config;
 try {
   config = JSON.parse(babelrc);
 } catch (err) {
-  console.error('==>     ERROR: Error parsing your .babelrc.');
+  console.error('==>     ERROR: Error parsing your 66666 .babelrc.');
   console.error(err);
 }
 
